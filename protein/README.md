77e808a3a7e5d50848e60200e6742952c852b557
Jose Labra <jelabra@gmail.com>
2023-06-28 03:58:25 +0200

    Added ttl version of protein.ttl

--- protein/README.md
+++ protein/README.md
@@ -1,6 +1,13 @@
 # Step by step instructions
 
+- Download Uniprot RDF files from <https://ftp.uniprot.org/pub/databases/uniprot/current_release/rdf/>
 
-## Requirements
+- Convert RDF/XML to N-Triples. We employed [Apache Jena](https://jena.apache.org/download/index.cgi) riot command line tool.  
+
+- Create subset using [pschema tool Rust tool](https://github.com/angelip2303/pschema-rs)
+
+```sh
+???
+```
 
 
---
+++ protein/rdf2ntriples.sh
@@ -0,0 +1,12 @@
+# ===============================================================================================
+# The following code converts the provided RDF/XML file into N-Triples format, assuming that the 
+# user has installed the apache-jena binaries. Please note that the result will be printed to the
+# Standard Output by the riot command, so we need to redirect it to a file.
+#
+# 1. (https://jena.apache.org/download/index.cgi#apache-jena-binary-distributions)
+# 2. Make sure you correctly configure the path to the apache-jena CLI tools installation. Set the
+# Environment Variable %JENA_HOME% to the Apache Jena installation folder and add the 
+# %JENA_HOME%\bat folder to the Path variable.
+# ===============================================================================================
+
+riot --output=ntriples uniprotkb_reviewed_viruses_10239.rdf > uniprotkb_reviewed_viruses_10239.nt
